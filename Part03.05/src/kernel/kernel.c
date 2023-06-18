7146b2f561eb74e3e045748093bcc460693ed608
webserver3315.macbook <webserver3315@naver.com>
2023-06-19 00:10:18 +0900

    Part04 Finished: mem.c mem.h atag.c atag.h list.h Added

--- Part03.05/src/kernel/kernel.c
+++ Part03.05/src/kernel/kernel.c
@@ -11,12 +11,15 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
     (void) r1;
     (void) atags;
 
+    char buf[256];
+
     uart_init();
-    uart_puts("Hello, kernel World!\r\n");
+    puts("Hello, kernel World!\r\n");
 
     while (1) {
-        // UART 응답받고 UART로 전송하고 반복
-        uart_putc(uart_getc());
-        uart_putc('\n');
+        // Echo 동작을 반복함
+        gets(buf, 256);
+        puts(buf);
+        putc('\n');
     }
 }
